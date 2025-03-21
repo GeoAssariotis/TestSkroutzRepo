3400132b0772097b74992797c207c8fe9529d583
Geo Assa <geo.assariotis@gmail.com>
2025-03-21 11:47:35 +0200

    Initial Commit

---
+++ MealPlan/MealPlan/MealPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlanApp.swift
+//  MealPlan
+//
+//  Created by linq20 on 21/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MealPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MealPlan/MealPlanTests/MealPlanTests.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlanTests.swift
+//  MealPlanTests
+//
+//  Created by linq20 on 21/3/25.
+//
+
+import Testing
+@testable import MealPlan
+
+struct MealPlanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
